2ebb58a1fa675705d3e8883d80be0f418ca1645f
Ramnath Shenoy <bekalshenoy@gmail.com>
2018-09-23 09:49:46 +0530

    menu is back

--- index.js
+++ index.js
@@ -1,56 +1,131 @@
+const {Menu} = require('electron')
 const electron = require('electron')
 const {app, BrowserWindow} = electron
 
 app.on('ready', () => {
   let win = new BrowserWindow({ width: 1100, height: 680, icon:'playork.png', title: "Playork", backgroundColor: '#111', show: false})
   win.loadURL(`file://${__dirname}/index.html`)
-  win.setMenuBarVisibility(false)//menu is in work
   win.on('ready-to-show', function() { 
   win.show(); 
   win.focus();
   });
 });
-//menu is in work
-if (process.platform === 'darwin') { 
-  var template = [{ 
-    label: 'FromScratch', 
-    submenu: [{ 
-      label: 'Quit', 
-      accelerator: 'CmdOrCtrl+Q', 
-      click: function() { 
-        app.quit(); 
-      } 
-    }] 
-  }, { 
-    label: 'Edit', 
-    submenu: [{ 
-      label: 'Undo', 
-      accelerator: 'CmdOrCtrl+Z', 
-      selector: 'undo:' 
-    }, { 
-      label: 'Redo', 
-      accelerator: 'Shift+CmdOrCtrl+Z', 
-      selector: 'redo:' 
-    }, { 
-      type: 'separator'
-    }, { 
-      label: 'Cut', 
-      accelerator: 'CmdOrCtrl+X', 
-      selector: 'cut:' 
-    }, { 
-      label: 'Copy', 
-      accelerator: 'CmdOrCtrl+C', 
-      selector: 'copy:'
-    }, { 
-      label: 'Paste', 
-      accelerator: 'CmdOrCtrl+V', 
-      selector: 'paste:' 
-    }, { 
-      label: 'Select All', 
-      accelerator: 'CmdOrCtrl+A', 
-      selector: 'selectAll:' 
-    }] 
-  }];
-  var osxMenu = menu.buildFromTemplate(template);
-  menu.setApplicationMenu(osxMenu);
+const template = [
+  {
+    label: 'Edit',
+    submenu: [
+      {
+        role: 'undo'
+      },
+      {
+        role: 'redo'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'cut'
+      },
+      {
+        role: 'copy'
+      },
+      {
+        role: 'paste'
+      },
+      {
+        role: 'pasteandmatchstyle'
+      },
+      {
+        role: 'delete'
+      },
+      {
+        role: 'selectall'
+      }
+    ]
+  },
+  {
+    label: 'View',
+    submenu: [
+      {
+        label: 'Reload',
+        accelerator: 'CmdOrCtrl+R',
+        click (item, focusedWindow) {
+          if (focusedWindow) focusedWindow.reload()
+        }
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'resetzoom'
+      },
+      {
+        role: 'zoomin'
+      },
+      {
+        role: 'zoomout'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        role: 'togglefullscreen'
+      }
+    ]
+  },
+  {
+    role: 'window',
+    submenu: [
+      {
+        role: 'minimize'
+      },
+      {
+        role: 'close'
+      }
+    ]
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Bug Report',
+        click () { require('electron').shell.openExternal('https://github.com/Playork/Playork/issues') }
+      },
+      {
+        label: 'Contact US',
+        click () { require('electron').shell.openExternal('https://bekalshenoy.wixsite.com/website/contact-1') }
+      }
+    ]
+  }
+]
+
+if (process.platform === 'darwin') {
+  const name = app.getName()
+  template.unshift({
+    label: name,
+    submenu: [
+      {
+        role: 'quit'
+      }
+    ]
+  })
+  template[3].submenu = [
+    {
+      label: 'Close',
+      accelerator: 'CmdOrCtrl+W',
+      role: 'close'
+    },
+    {
+      label: 'Minimize',
+      accelerator: 'CmdOrCtrl+M',
+      role: 'minimize'
+    },
+    {
+      label: 'Zoom',
+      role: 'zoom'
+    }
+  ]
 }
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
